0f04f6edcdafb3e2ae30501ceef33a442565a92f
sephv <sephvelut@gmail.com>
2018-06-19 14:32:25 -0400

    Update zsh-history-substring-search.zsh

--- zsh-history-substring-search.zsh
+++ zsh-history-substring-search.zsh
@@ -247,10 +247,10 @@ _history-substring-search-begin() {
     # `(j:CHAR:)` join array to string with CHAR as seperator
     #
     if [[ -z $HISTORY_SUBSTRING_SEARCH_FUZZY_AFTER_CURSOR ]]; then
-			local search_pattern="${(j:*:)_history_substring_search_query_parts[@]//(#m)[\][()|\\*?#<>~^]/\\$MATCH}*"
-		else
-			local search_pattern="*${(j:*:)_history_substring_search_query_parts[@]//(#m)[\][()|\\*?#<>~^]/\\$MATCH}*"
-		fi
+	local search_pattern="${(j:*:)_history_substring_search_query_parts[@]//(#m)[\][()|\\*?#<>~^]/\\$MATCH}*"
+    else
+    	local search_pattern="*${(j:*:)_history_substring_search_query_parts[@]//(#m)[\][()|\\*?#<>~^]/\\$MATCH}*"
+    fi
 
     #
     # Find all occurrences of the search pattern in the history file.
